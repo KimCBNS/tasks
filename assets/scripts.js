7d2cafdc16a0ea654cecd469c38ebf25574ca341
unknown <desveaux.kim@gmail.com>
2024-05-26 09:05:25 -0300

    modal data becomes an object ready for local storage step

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,24 +1,27 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList =  []; //JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+let addTaskBtn = $('#addTask');
 
-//listen for the button to add a task
-
+//adds function for the datepicker
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );
 
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
   // placeholder to use an Id...not sure how to use this yet.
-  
-  const newTaskId = $('<h1>').uniqueId();
-
+  // this will become part of the object of the task somehow
+  // this will be applied when the card is created not stored with the cards 
+  // so I will use it when I import and create the cards. This makes sense so it is done on the live feed not the stored feed so we can be sure they are unique. But it will have to be done each time a card is created.
+  this.uniqueId();
 }
 
 
 // Todo: create a function to create a task card
-// this should be first
-function createTaskCard(task) {
-
+function createTaskCard() {
+ 
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -27,7 +30,34 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
+// this is done when new task button is clicked
+// task needs to be added to the object in local storage
 function handleAddTask(event){
+  event.preventDefault();
+  console.log('clicked');
+  
+  const taskTitle = $('input[name="title"]').val();
+  const taskDescript = $('input[name="description"]').val();
+  const taskDate = $('input[name="date"]').val();
+  
+
+  // create an object from the data
+
+  
+  if (taskList.length === 0) {
+    taskList[0] = {tTitle: taskTitle, tDescription: taskDescript, tDate: taskDate, tId: generateTaskId};
+  } else {
+    employeesArray[employeesArray.length] = {tTitle: taskTitle, tDescription: taskDescript, tDate: taskDate};
+    }
+  
+    console.log(taskList);
+    // store this new object in local storage
+
+
+  // if (!taskTitle) {
+  //   console.log('No shopping item filled out in form!');
+  //   return;
+
 
 }
 
@@ -43,6 +73,8 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+// listen for the add task button to be clicked
+addTaskBtn.on('click', handleAddTask);
 
 });
 
